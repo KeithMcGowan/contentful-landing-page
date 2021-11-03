3c89dd239c43954094c229c217f6867d1b5b2833
Keith <keithrichardmcgowan@gmail.com>
2021-11-03 18:34:06 -0400

    Created hook with error handling.

--- src/components/MyName/MyName.jsx
+++ src/components/MyName/MyName.jsx
@@ -1,37 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useContentful } from "../../hooks/useContentful";
 import "./MyName.css";
 
 const query = `
 query {
   person2(id: "3lsIww5Gtd8YbhXsgXoQj5") {
     name
+    age
   }
 }
 `;
 
-const { REACT_APP_SPACE_ID, REACT_APP_CDA_TOKEN } = process.env;
-
 export const MyName = () => {
-  let [data, setData] = useState(null);
+  let { data, errors } = useContentful(query);
+  //   console.log(data.person2);
+  //   const { name, age } = data.person2;
 
-  useEffect(() => {
-    window
-      .fetch(
-        `https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_ID}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${REACT_APP_CDA_TOKEN}`,
-          },
-          body: JSON.stringify({ query }),
-        }
-      )
-      .then((res) => res.json())
-      .then((json) => setData(json.data));
-  }, []);
-
+  if (errors)
+    return (
+      <span className="error">
+        {errors.map((err) => err.message).join(",")}
+      </span>
+    );
   if (!data) return <span>Loading...</span>;
 
-  return <>{data.person2.name}</>;
+  return (
+    <>
+      Hi, I'm {data.person2.name} and I am {data.person2.age} years old.
+    </>
+  );
 };
